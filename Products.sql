63f5d4f85a785aec948be69ec2a6fd54999b8c83
Manasi Mhetre <manasimhetre1912@gmail.com>
2025-07-31 23:25:03 +0530

    Products.sql

--- Products.sql
+++ Products.sql
@@ -39,3 +39,17 @@ SELECT product_name, category,
 	  --ELSE 'Furniture Item'
 	END AS category_type
 FROM products;
+
+
+ALTER TABLE products
+ADD COLUMN discount_price NUMERIC(10, 2);
+
+UPDATE products
+SET discount_price = price*0.9  -- 10% discount
+WHERE product_name NOT IN ('Laptop', 'Desk')
+
+	
+/* COALESCE FUNCTION */
+SELECT product_name, price, discount_price, 
+	COALESCE (discount_price, price) AS Final_price
+FROM products
